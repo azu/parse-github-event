6855b4afb92fb564900f48a5f5126c09d04d434e
azu <azuciao@gmail.com>
2014-04-27 00:37:27 +0900

    fix(parse): fix error of range at GollumEvent

--- parse-github-event.js
+++ parse-github-event.js
@@ -180,7 +180,7 @@ function parse(event) {
                     },
                     // https://github.com/Constellation/escodegen/wiki/_compare/8071c6feb719b3c9e1742620aab9c1bbfda80e70...a567b1a221885a9ae5c576561e18ce68909624b6
                     html_url: GITHUB_DOMAIN + "/" + repo
-                        + "/wiki/_compare/" + event.payload.pages[0].sha + "..." + event.payload.pages[event.payload.pages.length].sha
+                        + "/wiki/_compare/" + event.payload.pages[0].sha + "..." + event.payload.pages[event.payload.pages.length-1].sha
                 };
             }
             break;
